a6b1f089ecd93b1a6c0277ad3993944a366daf60
MD-CodeWorks <muhammaddilawar749@gmail.com>
2026-08-14 23:10:49 +0500

    feat: add Exercise 8 - implement functionality to sort a list of numbers

---
+++ Python/Exercises/Python list Exercises 45 Coding Problems with Solutions/Exercise 8. Sort a List of Numbers.py
@@ -0,0 +1,18 @@
+"""
+    Date: 2026-08-14
+    Time: 23:07:52
+    Exercise 8. Sort a List of Numbers
+    Practice Problem: Sort a list of numbers in ascending order (lowest to highest).
+
+    Exercise Purpose: Sorting is perhaps the most studied topic in Computer Science. It turns chaotic data into organized data, which is a prerequisite for high-speed search algorithms like Binary Search. Python uses Timsort, an efficient, hybrid sorting algorithm.
+
+
+    Given Input: Unsorted: [56, 12, 89, 3, 22]
+    Expected Output: Sorted List: [3, 12, 22, 56, 89]
+"""
+
+Unsorted = [56, 12, 89, 3, 22]
+
+Unsorted.sort()
+
+print("Sorted List:", Unsorted)
